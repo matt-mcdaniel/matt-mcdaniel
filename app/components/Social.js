2c6d37c8b699bee4c6a4f3dac6a5cb744c1c08d3
Matt McDaniel <matt.h.mcdaniel@gmail.com>
2016-04-18 22:16:46 -0700

    load images

--- app/components/Social.js
+++ app/components/Social.js
@@ -6,25 +6,25 @@ class Social extends React.Component {
             {
                 text: 'LinkedIn',
                 href: 'https://www.linkedin.com/in/matthmcdaniel',
-                src: '../../img/linkedin-icon.svg'
+                src: '../../img/linkedin.svg'
             },
             {
                 text: 'Github',
                 href: 'https://github.com/matt-mcdaniel',
-                src: '../../img/github-icon.svg'
+                src: '../../img/github.svg'
             },
             {
                 text: 'Codepen',
                 href: 'http://codepen.io/himmel/',
-                src: '../img/codepen-icon.svg'
+                src: '../img/codepen.svg'
             }
         ];
         
         return (
             <div className="social-container">
-                {items.map(function(item) {
+                {items.map(function(item, i) {
                     let lower = item.text.toLowerCase();
-                    return <a className={'social ' + lower}
+                    return <a key={i} className={'social ' + lower}
                                 href={item.href}>
                             <img className="social__icon" 
                                 src={item.src} />
